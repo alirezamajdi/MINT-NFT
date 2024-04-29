47388472631ca7ecea22c3ab24960154abea46db
alirezamajdi <majdialireza01@gmail.com>
2024-04-29 13:26:52 +0330

    fix: this is ok

--- src/app/hi/page.tsx
+++ src/app/hi/page.tsx
@@ -5,10 +5,7 @@ import Draggable, { DraggableCore } from "react-draggable";
 const Page = () => {
   return (
     <div>
-      <Draggable>
-        <div>I can now be moved around!</div>
-      </Draggable>
-
+     
       <Draggable>
         <div>I can now be moved around!</div>
       </Draggable>
