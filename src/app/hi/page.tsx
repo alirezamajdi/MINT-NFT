075e1d6adcdc7d52c15323ec0bd7e04611f95b9a
alirezamajdi <majdialireza01@gmail.com>
2024-04-29 13:24:55 +0330

    feat: add page

---
+++ src/app/hi/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import React from "react";
+import Draggable, { DraggableCore } from "react-draggable";
+
+const Page = () => {
+  return (
+    <div>
+      <Draggable>
+        <div>I can now be moved around!</div>
+      </Draggable>
+
+      <Draggable>
+        <div>I can now be moved around!</div>
+      </Draggable>
+    </div>
+  );
+};
+
+export default Page;
