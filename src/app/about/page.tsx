39e0d23a77e243b6b621dc41de10033c2f40a00c
alirezamajdi <majdialireza01@gmail.com>
2024-04-29 13:00:33 +0330

    update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,16 +1,14 @@
-"use client"
+"use client";
 import React from "react";
 import Draggable, { DraggableCore } from "react-draggable";
 
 const Page = () => {
   return (
     <div>
-      <Draggable >
-        <div>I can now be moved around!</div>
-      </Draggable>
       <Draggable>
         <div>I can now be moved around!</div>
       </Draggable>
+
       <Draggable>
         <div>I can now be moved around!</div>
       </Draggable>
