d8ceb0bab05a720143676975dd3dc8a98d70282a
alirezamajdi <majdialireza01@gmail.com>
2024-04-29 13:21:01 +0330

    Fix

---
+++ commitlint.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    rules: {
+      'header-max-length': [2, 'always', 100],
+      // Other rules here
+    },
+  };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,6 @@ const inter = Inter({
   display: "swap",
 });
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
